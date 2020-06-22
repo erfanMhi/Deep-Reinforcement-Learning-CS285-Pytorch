3d8e895267b00fa2655022fd3eb70dddca62b69f
Erfan Miahi <mhi.erfan1@gmail.com>
2020-06-22 19:41:29 +0430

    Homework 2 is fully done.

---
+++ hw2/cs285/agents/base_agent.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class BaseAgent(object):
+    def __init__(self, **kwargs):
+        super(BaseAgent, self).__init__(**kwargs)
+
+    def train(self):
+        raise NotImplementedError
+
+    def add_to_replay_buffer(self, paths):
+        raise NotImplementedError
+
+    def sample(self, batch_size):
+        raise NotImplementedError
